5a70dc60b909dd246624afb0122e77b8982005ce
Chrisissorry <ch.tietz@gmail.com>
2021-11-25 21:09:37 +0100

    initial commit

---
+++ OutputHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DotnetScriptHelpers
+{
+    public class OutputHelper
+    {
+        public static void Success(string text)
+        {
+            WriteLine(text, ConsoleColor.Green);
+        }
+        
+        public static void Error(string text)
+        {
+            WriteLine(text, ConsoleColor.Red);
+        }
+
+        public static void Warning(string text)
+        {
+            WriteLine(text, ConsoleColor.DarkYellow);
+        }
+
+
+        public static void Info(string text)
+        {
+            WriteLine(text, ConsoleColor.DarkCyan);
+        }
+        
+        public static void WriteLine(string text, ConsoleColor? color = null)
+        {
+            if (color == null)
+            {
+                Console.WriteLine(text);
+                return;
+            }
+
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color.Value;
+            Console.WriteLine(text);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}

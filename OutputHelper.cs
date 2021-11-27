599db4ecc7d58c90f58875d1db74af2d3265df19
Chrisissorry <ch.tietz@gmail.com>
2021-11-27 11:34:06 +0100

    Adding methods which return directly

--- OutputHelper.cs
+++ OutputHelper.cs
@@ -4,25 +4,50 @@ namespace DotnetScriptHelpers
 {
     public class OutputHelper
     {
-        public static void Success(string text)
+        public static void WriteSuccess(string text)
         {
             WriteLine(text, ConsoleColor.Green);
         }
-        
-        public static void Error(string text)
+
+        public static void WriteError(string text)
         {
             WriteLine(text, ConsoleColor.Red);
         }
 
-        public static void Warning(string text)
+        public static void WriteWarning(string text)
         {
             WriteLine(text, ConsoleColor.DarkYellow);
         }
 
 
-        public static void Info(string text)
+        public static void WriteInfo(string text)
         {
             WriteLine(text, ConsoleColor.DarkCyan);
+        }
+        
+        public static int ReturnSuccess(string text)
+        {
+            WriteLine(text, ConsoleColor.Green);
+            return 0;
+        }
+        
+        public static int ReturnError(string text)
+        {
+            WriteLine(text, ConsoleColor.Red);
+            return 1;
+        }
+        
+        public static int ReturnWarning(string text)
+        {
+            WriteLine(text, ConsoleColor.DarkYellow);
+            return 0;
+        }
+
+
+        public static int ReturnInfo(string text)
+        {
+            WriteLine(text, ConsoleColor.DarkCyan);
+            return 0;
         }
         
         public static void WriteLine(string text, ConsoleColor? color = null)
